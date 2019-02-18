c3ba43b7fcf4eb3520fbd90666338fc2a6b408a7
xiaojian <theonlyz@foxmail.com>
2019-02-18 10:09:40 +0800

    #

--- projects/ngx-gannt/src/lib/ngx-gannt.component.ts
+++ projects/ngx-gannt/src/lib/ngx-gannt.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 
+
 @Component({
   selector: 'ngx-gannt',
-  templateUrl:'./ngx-gannt.component.html',
-  styleUrls:['ngx-gannt.scss']
+  templateUrl: './ngx-gannt.component.html',
+  styleUrls: ['ngx-gannt.scss'],
 })
 export class NgxGanntComponent implements OnInit {
   @HostBinding('class.ngx-gannt-wrap') isWrap = true;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
